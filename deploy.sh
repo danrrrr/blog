282b4aa9452bb19d0fc09fe91122ad57bc5962a4
danrrrr <851812859@qq.com>
2018-11-08 15:20:50 +0800

    push到新分支

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:danrrrr/danrrrr.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:danrrrr/blog.git master
+git push -f git@github.com:danrrrr/blog.git master:gh-pages
 
 cd -
