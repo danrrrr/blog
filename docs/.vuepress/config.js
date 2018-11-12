ecd853931eb46f589db7e2afde1e662e11a13050
huxiaodanr <851812859@qq.com>
2018-11-12 10:10:17 +0800

    markdown 笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     base: '/',
-    title: "胡小丹的学习笔记",
-    description: "Good good study, day day up! ",
+    title: '胡小丹的学习笔记',
+    description: 'Good good study, day day up! ',
     markdown: {
         lineNumbers: true
     },
@@ -22,6 +22,7 @@ module.exports = {
         ],
         sidebar: {
             '/blog/': [
+                'markdown',
                 'H5_wxShare',
                 'js_prototype',
             ],
